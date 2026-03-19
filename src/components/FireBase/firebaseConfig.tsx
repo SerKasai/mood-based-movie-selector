f7dc6909ec0f4134c3c68914fd2d9f1d5ab34e06
Sergio Ignizio <s.ignizio@ext.nextmind.it>
2026-03-19 12:35:48 +0100

    create .env.local

--- src/components/FireBase/firebaseConfig.tsx
+++ src/components/FireBase/firebaseConfig.tsx
@@ -6,13 +6,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBR65wQ-AHhxlItPtzfMcaUj8QDhg1_NJw",
-  authDomain: "moodflix-login.firebaseapp.com",
-  projectId: "moodflix-login",
-  storageBucket: "moodflix-login.firebasestorage.app",
-  messagingSenderId: "953123405184",
-  appId: "1:953123405184:web:3115898ca0264278389565",
-  measurementId: "G-TMFX2GQG65",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
